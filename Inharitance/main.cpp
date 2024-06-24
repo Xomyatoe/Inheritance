955bf3e3841b152ce2c1879a238ba29503c2b12d
Seva <Seva@DESKTOP-PLC715R>
2024-06-24 22:57:54 +0500

    Before ostream overload

--- Inharitance/main.cpp
+++ Inharitance/main.cpp
@@ -224,7 +224,8 @@ void main()
 	cout << delimiter << endl;
 	for (int i = 0; i < sizeof(group) / sizeof(group[0]); i++)
 	{
-		group[i]->info();
+		//group[i]->info();
+		cout << *group[i] << endl;
 		cout << delimiter << endl;
 	}
 
